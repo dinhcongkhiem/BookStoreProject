b4bbbcda729bda2c9272afe83c9fc690a108baad
Khiemdc <khiemdcph33789@fpt.edu.vn>
2024-07-17 21:49:56 +0700

    project initialization

---
+++ book_store_be/src/main/java/com/project/book_store_be/BookStoreBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.book_store_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreBeApplication.class, args);
+	}
+
+}
---
+++ book_store_be/src/test/java/com/project/book_store_be/BookStoreBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.book_store_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
