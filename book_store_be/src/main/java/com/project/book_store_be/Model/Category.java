2e6972a6dba9056b34a4d1a53c6bc7aed5dc968b
xuantruong181 <lexuantruong18012004@gmail.com>
2024-07-27 22:53:15 +0700

    Add Category API - BE

---
+++ book_store_be/src/main/java/com/project/book_store_be/Model/Category.java
@@ -0,0 +1,20 @@
+package com.project.book_store_be.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ book_store_be/src/main/java/com/project/book_store_be/Repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.project.book_store_be.Repository;
+
+import com.project.book_store_be.Model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+    Optional<Category> findByName(String name);
+}
