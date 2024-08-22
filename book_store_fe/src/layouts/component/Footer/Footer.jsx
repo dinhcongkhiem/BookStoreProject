5146ca9ef7c050f0e8f2accffefde72153b5be98
Duchi <ducbv74@gmail.com>
2024-08-22 22:53:38 +0700

    Em khong bi dien nha

--- book_store_fe/src/layouts/component/Footer/Footer.jsx
+++ book_store_fe/src/layouts/component/Footer/Footer.jsx
@@ -1,9 +1,89 @@
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
 import style from './Footer.module.scss';
+import logoBook from '../../../assets/image/Logo-BookBazaar-nobg.png';
 
 const cx = classNames.bind(style);
+
 function Footer() {
-    return <h2>Footer ddaay nerf</h2>;
+    return (
+        <footer className={cx('footerWrapper')}>
+            <div className={cx('footerArea')}>
+                <div className={cx('customContainer')}>
+                    <div className={cx('customRow')}>
+                        <div className={cx('colLarge', 'colMedium', 'colSmall')}>
+                            <div className={cx('singleFooterCaption')}>
+                                <div className={cx('footerLogo')}>
+                                    <a href="/">
+                                        <img src={logoBook} alt="Logo" style={{ width: '40px', height: 'auto' }} />
+                                    </a>
+                                </div>
+                                <div className={cx('footerTittle')}>
+                                    <p>
+                                        Get the breathing space now, and we’ll extend your term at the other end year for go.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx('colMedium', 'colSmall')}>
+                            <div className={cx('singleFooterCaption')}>
+                                <div className={cx('footerTittle')}>
+                                    <h4>Book Category</h4>
+                                    <ul>
+                                        <li><a href="#">History</a></li>
+                                        <li><a href="#">Horror - Thriller</a></li>
+                                        <li><a href="#">Love Stories</a></li>
+                                        <li><a href="#">Science Fiction</a></li>
+                                        <li><a href="#">Business</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx('colMedium', 'colSmall')}>
+                            <div className={cx('singleFooterCaption')}>
+                                <div className={cx('footerTittle')}>
+                                    <h4>Other Categories</h4>
+                                    <ul>
+                                        <li><a href="#">Biography</a></li>
+                                        <li><a href="#">Astrology</a></li>
+                                        <li><a href="#">Digital Marketing</a></li>
+                                        <li><a href="#">Software Development</a></li>
+                                        <li><a href="#">Ecommerce</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx('colLarge', 'colMedium', 'colSmall')}>
+                            <div className={cx('singleFooterCaption')}>
+                                <div className={cx('footerTittle')}>
+                                    <h4>Site Map</h4>
+                                    <ul>
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="#">FAQs</a></li>
+                                        <li><a href="#">Blog</a></li>
+                                        <li><a href="#">Contact</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cx('footerBottomArea')}>
+                <div className={cx('customContainer')}>
+                    <div className={cx('footerBorder')}>
+                        <div className={cx('customRow')}>
+                            <div className={cx('colFull')}>
+                                <div className={cx('footerCopyRight')}>
+                                    &copy; 2024 All rights reserved | This template is made with <i className="fa fa-heart" aria-hidden="true"></i> by <a href="#" target="_blank" rel="nofollow noopener">Chicken</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- book_store_fe/src/layouts/component/Header/Header.jsx
+++ book_store_fe/src/layouts/component/Header/Header.jsx
@@ -1,12 +1,55 @@
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import logoBook from '../../../assets/image/Logo-BookBazaar-nobg.png';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const cx = classNames.bind(style);
+
 function Header() {
     return (
-        <div className={cx('wrapper')}>
-            <h2>Header</h2>
-        </div>
+        <header className={cx('headerWrapper')}>
+            <div className={cx('container')}>
+                <div className={cx('logo')}>
+                    <Link to="/">
+                        <img src={logoBook} alt="BookStore Logo" className={cx('logoImage')} />
+                    </Link>
+                </div>
+                <div className={cx('nameBrand')}>
+                    <h4>Book Bazaar</h4>
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Tìm kiếm sách..." className={cx('searchInput')} />
+                    <FontAwesomeIcon icon={faSearch} className={cx('searchIcon')} />
+                </div>
+                <div className={cx('cart')}>
+                    <FontAwesomeIcon icon={faShoppingCart} size="lg" />
+                </div>
+                <div className={cx('signin')}>
+                    <Link to="/login">
+                        <button type="button" className={cx('signinButton')}>Đăng nhập</button>
+                    </Link>
+                </div>
+            </div>
+
+            <Navbar bg="light" expand="lg" className={cx('menuBar')}>
+                <Container>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+                        <Nav>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/categories">Categories</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/pages">Pages</Nav.Link>
+                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     );
 }
 
