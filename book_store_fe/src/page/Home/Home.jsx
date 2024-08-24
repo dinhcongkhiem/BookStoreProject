264f2a737abe15364373cf2c51ace6f2a095ac0f
Khiemdc <khiemdcph33789@fpt.edu.vn>
2024-08-24 16:05:21 +0700

    update header footer

--- book_store_fe/src/page/Home/Home.jsx
+++ book_store_fe/src/page/Home/Home.jsx
@@ -1,6 +1,7 @@
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import classNames from 'classnames/bind';
 
+import style from './Home.module.scss'
+const cx = classNames.bind(style);
 function Home() {
     return (
         <div>
