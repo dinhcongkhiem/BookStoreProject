83821f5b3e6af00a3e5930fff05edb39caf5f8f8
Khiemdc <khiemdcph33789@fpt.edu.vn>
2024-08-27 12:06:19 +0700

    update security config

--- book_store_be/src/main/java/com/project/book_store_be/Controller/UserController.java
+++ book_store_be/src/main/java/com/project/book_store_be/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.project.book_store_be.Controller;
 
 import com.project.book_store_be.Request.ChangePasswordRequest;
 import com.project.book_store_be.Request.UpdateUserRequest;
+import com.project.book_store_be.Response.UserResponse;
 import com.project.book_store_be.Services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-
-    public UserController(UserService userService) {
-        this.userService = userService;
+    @GetMapping()
+    public ResponseEntity<UserResponse> getUser() {
+        return ResponseEntity.ok(userService.getUserInfor(userService.getCurrentStudent()));
     }
-
     @PatchMapping()
     public ResponseEntity<String> updateUser(@RequestBody UpdateUserRequest updateUserRequest){
         userService.updateUser(updateUserRequest);
