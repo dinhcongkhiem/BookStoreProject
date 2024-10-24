50824c64ea7da590bd4bf8ec05eaec129bf16f3c
dinhcongkhiem <85293370+dinhcongkhiem@users.noreply.github.com>
2024-10-24 19:59:15 +0700

    Update ShippingService.java

--- book_store_be/src/main/java/com/project/book_store_be/Interface/ShippingService.java
+++ book_store_be/src/main/java/com/project/book_store_be/Interface/ShippingService.java
@@ -4,4 +4,5 @@ import com.project.book_store_be.Response.FeeResponse;
 
 public interface ShippingService {
      FeeResponse calculateShippingFee(int weight, int value, String deliverOption);
+     FeeResponse calculateShippingFee(String province, String district,String ward,String address, int weight, int value, String deliverOption);
 }
