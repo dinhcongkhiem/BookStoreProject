cceb2b1b1f52fc022ce661e1ae6d32e59f96baef
Hue1601 <phamthihue3340@gmail.com>
2024-10-30 21:21:57 +0700

    get order detail by id

--- book_store_be/src/main/java/com/project/book_store_be/Interface/OrderService.java
+++ book_store_be/src/main/java/com/project/book_store_be/Interface/OrderService.java
@@ -3,6 +3,7 @@ package com.project.book_store_be.Interface;
 import com.project.book_store_be.Enum.OrderStatus;
 import com.project.book_store_be.Request.OrderRequest;
 import com.project.book_store_be.Response.OrderRes.CreateOrderResponse;
+import com.project.book_store_be.Response.OrderRes.OrderDetailResponse;
 import com.project.book_store_be.Response.OrderRes.OrderResponse;
 import org.springframework.data.domain.Page;
 
@@ -12,4 +13,6 @@ public interface OrderService {
     Page<?> getOrdersByUser(Integer page, Integer pageSize, OrderStatus status, String keyword);
     CreateOrderResponse createOrder(OrderRequest request);
     OrderStatus checkStatus(Long id);
+
+    OrderDetailResponse getOrderDetailById(Long id);
 }
--- book_store_be/src/main/java/com/project/book_store_be/Response/OrderRes/OrderDetailResponse.java
+++ book_store_be/src/main/java/com/project/book_store_be/Response/OrderRes/OrderDetailResponse.java
@@ -1,4 +1,25 @@
 package com.project.book_store_be.Response.OrderRes;
 
+import com.project.book_store_be.Enum.OrderStatus;
+import com.project.book_store_be.Enum.PaymentType;
+import com.project.book_store_be.Model.Address;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
 public class OrderDetailResponse {
+    private Long orderId;
+    private OrderStatus status;
+    private BigDecimal finalPrice;
+    private String fullname;
+    private String phoneNum;
+    private Address address;
+    private List<OrderItemsDetailResponse> items;
+    private PaymentType paymentType;
 }
