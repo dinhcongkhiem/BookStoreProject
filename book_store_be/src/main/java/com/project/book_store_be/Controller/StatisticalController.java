5b9027c3cc2d1f855b7a760ab6e92a3e83135c33
Binss04 <xuanthanh130304@gmail.com>
2024-11-14 16:06:06 +0700

    thongke

---
+++ book_store_be/src/main/java/com/project/book_store_be/Controller/StatisticalController.java
@@ -0,0 +1,34 @@
+package com.project.book_store_be.Controller;
+
+import com.project.book_store_be.Services.RevenueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/orders")
+public class StatisticalController {
+    @Autowired
+    private RevenueService revenueService;
+
+    @GetMapping("/monthly-revenue-profit")
+    public List<Map<String, BigDecimal>> getMonthlyRevenueAndProfit() {
+        return revenueService.calculateMonthlyRevenueAndProfit();
+    }
+
+    @GetMapping("/current-week")
+    public List<Map<String, Object>> getDailyRevenueAndProfitForCurrentWeek() {
+        return revenueService.calculateDailyRevenueAndProfitForCurrentWeek();
+    }
+
+    @GetMapping("/statistics")
+    public Map<String, Object> getOrderStatistics() {
+        return revenueService.getOrderStatistics();
+    }
+}
--- book_store_be/src/main/java/com/project/book_store_be/Repository/OrderRepository.java
+++ book_store_be/src/main/java/com/project/book_store_be/Repository/OrderRepository.java
@@ -5,7 +5,10 @@ import com.project.book_store_be.Model.Order;
 import com.project.book_store_be.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +16,10 @@ import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
     List<Order> findByStatus(OrderStatus status);
+
+    @Query("SELECT o FROM Order o WHERE o.status = :status AND o.orderDate BETWEEN :startDate AND :endDate")
+    List<Order> findByStatusAndDateRange(OrderStatus status, LocalDateTime startDate, LocalDateTime endDate);
+
+    List<Order> findByOrderDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
 }
