c8a60af8da3b888e9efb5f7e0af8beb4008272b4
xuantruong181 <lexuantruong18012004@gmail.com>
2024-11-20 20:07:11 +0700

    checkUserReviewedProducts

--- book_store_be/src/main/java/com/project/book_store_be/Repository/ReviewRepository.java
+++ book_store_be/src/main/java/com/project/book_store_be/Repository/ReviewRepository.java
@@ -4,6 +4,8 @@ import com.project.book_store_be.Model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +14,6 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
     Page<Review> findByProductId(Long productId, Pageable pageable);
     List<Review> findByProductId(Long productId);
     int countByProductId(Long productId);
+    @Query("SELECT r.product.id FROM Review r WHERE r.user.id = :userId AND r.product.id IN :productIds")
+    List<Long> findReviewedProductIdsByUser(@Param("userId") Long userId, @Param("productIds") List<Long> productIds);
 }
